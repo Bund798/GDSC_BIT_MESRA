7399b77ca86589050f314f312de30b8f412e9025
Bund798 <79251188+Bund798@users.noreply.github.com>
2021-09-18 23:44:29 +0530

    the template

---
+++ main.js
@@ -0,0 +1,59 @@
+
+
+$('#learn').on("mouseover",function(){
+   var url="./images/learn.jpeg";
+   $(".container").css(
+      {"background":"no-repeat center fixed",
+  "background-size": "cover",
+  "background-image":" url("+url+")"
+}
+   );
+
+});
+$('#grow').on("mouseover",function(){
+   var url="./images/grow.jpeg";
+   $(".container").css(
+      {"background":"no-repeat center fixed",
+  "background-size": "cover",
+  "background-image":" url("+url+")"
+}
+   );
+
+});
+$('#connect').on("mouseover",function(){
+   var url="./images/connect.jpeg";
+   $(".container").css(
+      {"background":"no-repeat center fixed",
+  "background-size": "cover",
+  "background-image":" url("+url+")"
+}
+   );
+
+});
+$('.container').on("mouseover",function(){
+   var url="";
+   $(".container").css(
+      {
+  "background-image":" url("+url+")",
+  "background-color":"#D7E9F7"
+}
+   );
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
